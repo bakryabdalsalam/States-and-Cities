3eb6557ca333fe6465310948e1bd1b771c49cb32
Bakry Abdalsalam <67397650+bakryabdalsalam@users.noreply.github.com>
2024-06-04 10:01:11 +0000

    chore: Update city dropdown in admin custom script to dynamically replace with input field if no cities available for selected country

--- js/admin-custom-script.js
+++ js/admin-custom-script.js
@@ -1,9 +1,4 @@
 jQuery(document).ready(function($) {
-    // Ensure Select2 is loaded
-    if (typeof $.fn.select2 === 'undefined') {
-        return;
-    }
-
     // Only execute if the citiesData is available
     if (typeof citiesData === 'undefined') {
         return;
@@ -11,17 +6,10 @@ jQuery(document).ready(function($) {
 
     // Function to create dropdown
     function createCityDropdown(cities, fieldName) {
-        var cityDropdown = $('<select></select>').attr('name', fieldName).attr('id', fieldName).attr('class', 'select2');
+        var cityDropdown = $('<select></select>').attr('name', fieldName).attr('id', fieldName);
         $.each(cities, function(index, city) {
             cityDropdown.append($('<option></option>').attr('value', city).text(city));
         });
-
-        // Transform the dropdown into a searchable dropdown using Select2
-        cityDropdown.select2({
-            placeholder: 'Select a city',
-            allowClear: true
-        });
-
         return cityDropdown;
     }
 
@@ -31,30 +19,24 @@ jQuery(document).ready(function($) {
             // Initial replacement
             var initialCountry = countryField.val();
             if (initialCountry && citiesData[initialCountry]) {
-                var cityDropdown = createCityDropdown(citiesData[initialCountry], fieldName);
-                cityField.replaceWith(cityDropdown);
-                cityField = $('#' + fieldName); // Update cityField to refer to the new dropdown
+                cityField.replaceWith(createCityDropdown(citiesData[initialCountry], fieldName));
             }
 
             // Update city dropdown on country change
             countryField.on('change', function() {
                 var selectedCountry = $(this).val();
                 if (citiesData[selectedCountry]) {
-                    var cityDropdown = createCityDropdown(citiesData[selectedCountry], fieldName);
-                    cityField.replaceWith(cityDropdown);
-                    cityField = $('#' + fieldName); // Update cityField to refer to the new dropdown
+                    cityField.replaceWith(createCityDropdown(citiesData[selectedCountry], fieldName));
                 } else {
-                    var cityInput = $('<input>').attr('type', 'text').attr('name', fieldName).attr('id', fieldName);
-                    cityField.replaceWith(cityInput);
-                    cityField = $('#' + fieldName); // Update cityField to refer to the new input field
+                    cityField.replaceWith('<input type="text" name="' + fieldName + '" id="' + fieldName + '" />');
                 }
             });
         }
     }
 
     // Billing city field
-    replaceCityField($('#_billing_city_field input'), $('#_billing_country'), '_billing_city');
+    replaceCityField($('#_billing_city'), $('#_billing_country'), '_billing_city');
 
     // Shipping city field
-    replaceCityField($('#_shipping_city_field input'), $('#_shipping_country'), '_shipping_city');
+    replaceCityField($('#_shipping_city'), $('#_shipping_country'), '_shipping_city');
 });
